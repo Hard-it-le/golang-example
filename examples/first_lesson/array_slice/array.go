e7677b663fff9513814b704df41cf64c00e98895
yujiale <yujiale@authing.cn>
2022-08-24 23:15:38 +0800

    feat: go array ans slice (数组和切片） example

--- examples/first_lesson/array_slice/array.go
+++ examples/first_lesson/array_slice/array.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+/*
+*
+数组和别的语言的数组差不多，语法是：[cap]type
+
+1。 初始化要指定长度（或者叫做容量）
+
+2。直接初始化
+
+3。 arr[i]的形式访问元素
+
+4。 len 和 cap 操作用于获取数组长度
+*/
 func main() {
 	// 直接初始化一个三个元素的数组。大括号里面多一个或者少一个都编译不通过
 	a1 := [3]int{9, 8, 7}
-	fmt.Printf("a1: %v, len: %d, cap: %d", a1, len(a1), cap(a1))
+	fmt.Printf("a1: %v, len: %d, cap: %d \n", a1, len(a1), cap(a1))
 
 	// 初始化一个三个元素的数组，所有元素都是0
 	var a2 [3]int
-	fmt.Printf("a2: %v, len: %d, cap: %d", a2, len(a2), cap(a2))
+	fmt.Printf("a2: %v, len: %d, cap: %d \n", a2, len(a2), cap(a2))
 
 	//a1 = append(a1, 12) 数组不支持 append 操作
 
--- examples/first_lesson/array_slice/slice.go
+++ examples/first_lesson/array_slice/slice.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
+/**
+数据和切片的区别
+			数组     	切片
+直接初始化	支持			支持
+make		不支持		支持
+访问元素  	arr[i] 		arr[i]
+len			长度			已有元素个数
+cap			长度			容量
+append		不支持		支持
+是否可以扩容	不可以   	可以
+*/
+
+/**
+切片
+
+切片,语法:[]type
+
+1.直接初始化
+
+2. make初始化:make([]type, length, capacity)
+
+3. arr[i]的形式访问元素
+
+4. append 追加元素
+
+5. len 获取元素数量
+
+6.cap 获取切片容容量
+
+7. 推荐写法:s1:= make([]type. 0. capacity)
+
+如何理解切片
+
+1。 切片操作是有限的，不支持随机增州（即没有 add， delete 方法，需要自己写代码）
+
+2。 只有 append 操作
+
+3。 切片支持子切片操作，和原本切片是共享底层数组
+*/
+
 func main() {
 	s1 := []int{1, 2, 3, 4} // 直接初始化了 4 个元素的切片
 	fmt.Printf("s1: %v, len %d, cap: %d \n", s1, len(s1), cap(s1))
@@ -28,9 +68,20 @@ func main() {
 
 	// SubSlice()
 
-	//shareArr()
+	ShareSlice()
 }
 
+// SubSlice /**
+/**
+子切片
+数组和切片都可以通过[start：end ]的形式来获取子切片：
+
+1。 arr[start：end]，获得[start， end)之闻的元素
+
+2。 arr[：end]，获得[O， end) 之间的元素
+
+3。 arr[start:]，获得[start， len(arr)之间的元素
+*/
 func SubSlice() {
 	s1 := []int{2, 4, 6, 8, 10}
 	s2 := s1[1:3]
@@ -42,6 +93,17 @@ func SubSlice() {
 	s4 := s1[:3]
 	fmt.Printf("s4: %v, len %d, cap: %d \n", s4, len(s4), cap(s4))
 }
+
+// ShareSlice	/**
+/**
+共享底层的含义就是共享底层数组
+核心：共享数组
+
+子切片和切片究竟会不会互相影响，就抓住一点：它们是不是还共享数组？
+就是如果它们结构没有变化，那肯定是共享的；
+
+但是结构变化了，就可能不是共享了
+*/
 
 func ShareSlice() {
 
