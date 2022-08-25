6e932503fa3d7d1b7b9208186eaa3cc8f57a1843
yujiale <yujiale@authing.cn>
2022-08-25 19:56:19 +0800

    feat: go after class slice example1

--- examples/first_lesson/afterclass/slice.go
+++ examples/first_lesson/afterclass/slice.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	s := []int{1, 2, 4, 7}
 	// 结果应该是 5, 1, 2, 4, 7
@@ -23,11 +25,16 @@ func main() {
 }
 
 func Add(s []int, index int, value int) []int {
-	//TODO
+	fmt.Printf("s add before : %s\n", s)
+	s = append(s[:index], append([]int{value}, s[index:]...)...)
+	fmt.Printf("s add after : %s\n", s)
 	return s
 }
 
 func Delete(s []int, index int) []int {
+	fmt.Printf("s delete before : %s\n", s)
+	s = append(s[:index], s[index+1:]...)
+	fmt.Printf("s delete after : %s\n", s)
 	// TODO
 	return s
 }
--- examples/first_lesson/array_slice/slice.go
+++ examples/first_lesson/array_slice/slice.go
@@ -42,6 +42,25 @@ append		不支持		支持
 3。 切片支持子切片操作，和原本切片是共享底层数组
 */
 
+/*
+*
+
+切片常用操作
+1. 将切片 b 的元素追加到切片 a 之后： a = append(a, b...)
+2. 复制切片 a 的元素到新的切片 b 上：
+b = make([]T, len(a))
+copy(b, a)
+3. 删除位于索引 i 的元素： a = append(a[:i], a[i+1:]...)
+4. 切除切片 a 中从索引 i 至 j 位置的元素： a = append(a[:i], a[j:]...)
+5. 为切片 a 扩展 j 个元素长度： a = append(a, make([]T, j)...)
+6. 在索引 i 的位置插入元素 x： a = append(a[:i], append([]T{x}, a[i:]...)...)
+7. 在索引 i 的位置插入长度为 j 的新切片：
+a = append(a[:i], append(make([]T, j), a[i:]...)...)
+8. 在索引 i 的位置插入切片 b 的所有元素： a = append(a[:i], append(b, a[i:]...)...)
+9. 取出位于切片 a 最末尾的元素 x： x, a = a[len(a)-1], a[:len(a)-1]
+10. 将元素 x 追加到切片 a： a = append(a, x)
+因此，您可以使用切片和 append 操作来表示任意可变长度的序列。
+*/
 func main() {
 	s1 := []int{1, 2, 3, 4} // 直接初始化了 4 个元素的切片
 	fmt.Printf("s1: %v, len %d, cap: %d \n", s1, len(s1), cap(s1))
