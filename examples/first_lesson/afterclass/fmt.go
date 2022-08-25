d2d8bb98dd7120ce2c1b75b24d67f494c5b03588
yujiale <yujiale@authing.cn>
2022-08-25 18:58:53 +0800

    feat: go after class example1

--- examples/first_lesson/afterclass/fmt.go
+++ examples/first_lesson/afterclass/fmt.go
@@ -1,14 +1,36 @@
 package main
 
-func main() {
+import (
+	"encoding/hex"
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	const f1 = 12.1123
+	with2 := printNumWith2(f1)
+	fmt.Printf("with2 %v:\n", with2)
+	fmt.Printf("================== \n")
+
+	const test string = "12312312"
+	decodeString, _ := hex.DecodeString(test)
+
+	fmt.Printf("string  byte : %s \n", decodeString)
+	bytes := printBytes(decodeString)
+
+	fmt.Printf("bytes : %s \n\n", bytes)
 }
 
 // 输出两位小数
 func printNumWith2(float642 float64) string {
-	return ""
+	value, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", float642), 64)
+	fmt.Println(value)
+	sprintf := fmt.Sprintf("value : %s \n", value)
+	return sprintf
 }
 
+// 将[]byte  输出为16进制
 func printBytes(data []byte) string {
-	return ""
+	toString := hex.EncodeToString(data)
+	return toString
 }
